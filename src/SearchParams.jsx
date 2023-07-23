fe53ea272023605edf7735e37a793bdf31b09908
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-23 11:20:41 +0200

    setState part 2

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -10,6 +14,40 @@ const SearchParams = () => {
           Location
           <input onChange={e => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+          </label>
+          <label htmlFor="breed">
+          Breed
+          <select
+            disabled={breeds.length === 0}
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map(breed => (
+              <option key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+          </label>
         <button>Submit</button>
       </form>
     </div>
